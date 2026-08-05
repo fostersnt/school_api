e18a86aa5dd04ee4238c2cb3495b6a58d5209f41
Foster Amponsah <foster.asante@dictumlogic.com>
2026-08-05 10:56:07 +0000

    Update

--- commons/Util.py
+++ commons/Util.py
@@ -1,11 +1,13 @@
 import re
 
 class CustomUtility:
+    @staticmethod
     def validateMsisdn(msisdn: str):
         pattern = r'^233[25][34567]\d{7}$'
         result = re.fullmatch(pattern=pattern, string=msisdn)
-        return result
+        return bool(result)
 
+    @staticmethod
     def apiResponseFormat(success: bool, message: str, data: list):
         return {
             "success": success,
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from fastapi.exceptions import HTTPException as FastAPIHTTPException
 # Initialize FastAPI instance (backed by Starlette)
 app = FastAPI(title="Item Management API", version="1.0.0")
 
+
 #! Validation errors handler
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
@@ -23,21 +24,19 @@ async def validation_exception_handler(request, exc):
         content={"success": False, "message": "Validation failed", "errors": errors},
     )
 
+
 #! Exception handler
 @app.exception_handler(FastAPIHTTPException)
 async def http_exception_handler(request, exc):
     return JSONResponse(
-        status_code=exc.status_code,
-        content={
-            "success": False,
-            "message": exc.detail
-        }
+        status_code=exc.status_code, content={"success": False, "message": exc.detail}
     )
+
 
 class ItemBase(BaseModel):
     name: str = Field(..., example="Wireless Mouse")
     age: int = Field(...)
-    level: str = Field(..., min_length=2, example="A fast 2.4GHz mouse")
+    msisdn: str = Field(..., min_length=12, max_length=12, example="233xxxxxxxxx")
     # level: Optional[str] = Field(None, example="A fast 2.4GHz mouse")
     # price: float = Field(..., gt=0, example=29.99)
 
@@ -56,9 +55,15 @@ id_counter = 0
 
 @app.post("/items", response_model=ItemResponse, status_code=status.HTTP_201_CREATED)
 def create_item(item: ItemCreate):
-    raise HTTPException(status_code=500, detail=CustomUtility.apiResponseFormat(False, 'Testing the api', []))
-    global id_counter
-    new_item = ItemResponse(id=id_counter, **item.model_dump())
-    students.append(new_item)
-    id_counter += 1
-    return new_item
+    msisdn_check = CustomUtility.validateMsisdn(item.msisdn)
+    if msisdn_check == False:
+        raise HTTPException(
+            status_code=500,
+            detail=CustomUtility.apiResponseFormat(False, "Invalid msisdn", []),
+        )
+    else:
+        global id_counter
+        new_item = ItemResponse(id=id_counter, **item.model_dump())
+        students.append(new_item)
+        id_counter += 1
+        return new_item
